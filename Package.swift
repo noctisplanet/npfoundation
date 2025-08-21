228a247ed4f62b9afed6be00228d476f09c17222
jon <Jonathan.Kaishan@gmail.com>
2025-08-21 22:29:04 +0800

    Remove the NPFoundationExamples module.

--- Package.swift
+++ Package.swift
@@ -12,24 +12,14 @@ let package = Package(
             targets: [
                 "NPFoundation"
             ]),
-        .executable(
-            name: "NPFoundationExamples",
-            targets: [
-                "NPFoundationExamples"
-            ])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "NPFoundation"),
-        
         .testTarget(
             name: "NPFoundationTests",
-            dependencies: ["NPFoundation"]),
-        
-        .executableTarget(
-            name: "NPFoundationExamples",
             dependencies: ["NPFoundation"]),
     ]
 )
